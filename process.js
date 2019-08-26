6d0fe8bd3755207815343910095508354b2f0a42
Pathran-phantom1823 <patrick.pathran23@gmail.com>
2019-08-26 18:36:55 +0800

    constanst ID

--- process.js
+++ process.js
@@ -1,6 +1,6 @@
-
+var counter = 1;
 $(document).ready(function(){
-	var counter = 1;
+	
 	var curr_row = null;
 	$('#save').click(function(){
 		var Uid = $('input[name = "id"]').val();
@@ -8,18 +8,22 @@ $(document).ready(function(){
 		var coarsed = $('input[name="cy"]').val();
 		var addrs = $('input[name = "address"]').val();
 		var eml = $('input[name = "email"]').val(); 
-		var markuped = "<tr data-name='name' data-cy = 'cy'><td name='id' class = 'id' id= 'Uid' value ="+counter+">"+(counter++)+
+		var markuped = "<tr data-name='name' data-cy = 'cy'><td name='id' class = 'id' id= 'Uid'	>"+(counter)+
 						"</td><td name='fname' class = 'fname'>" + named + "</td><td name='cy' class = 'cy'>" + coarsed +"</td>" +
 						"<td name='address' class='address' style= 'display:none'>"+addrs+ 
 						"</td><td name='email' class='email' style= 'display:none'>"+eml+ "</td><td>" + 
 						"<button type = 'button' class='btn btn-primary' id = 'edit' style = 'display:none'>Edit</button> "+
 						" <button type = 'button' class='btn btn-warning' id= 'delete' style = 'display:none'>Delete</button>";
 
-		if(curr_row){
-			$('table tbody').find($(curr_row)).replaceWith(markuped);
-				curr_row = null;		
+		if(curr_row){	
+			curr_row = null;
 		}else{
-			$('table tbody').append(markuped);
+			$('table tbody').append("<tr data-name='name' data-cy = 'cy'><td name='id' class = 'id' id= 'Uid'	>"+(counter++)+
+						"</td><td name='fname' class = 'fname'>" + named + "</td><td name='cy' class = 'cy'>" + coarsed +"</td>" +
+						"<td name='address' class='address' style= 'display:none'>"+addrs+ 
+						"</td><td name='email' class='email' style= 'display:none'>"+eml+ "</td><td>" + 
+						"<button type = 'button' class='btn btn-primary' id = 'edit' style = 'display:none'>Edit</button> "+
+						" <button type = 'button' class='btn btn-warning' id= 'delete' style = 'display:none'>Delete</button>");
 		}
 		$("#id").val('');
 		$("#fname").val('');
@@ -66,7 +70,7 @@ $(document).ready(function(){
 
 	$('body').on('click','#edit', function(){
 			$('#form').modal();
-			$('#save').attr({'id':'update', 'class':'btn btn-primary'});
+			$('#save').attr({'id':'update', 'class':'btn btn-info'});
 			counter--;
 			curr_row = $(this).parents('tr');
 			$('#id').val($(this).closest('tr').find('td.id').text());
@@ -74,6 +78,13 @@ $(document).ready(function(){
 			$('#cy').val($(this).closest('tr').find('td.cy').text());
 			$('#address').val($(this).closest('tr').find('td.address').text());
 			$('#email').val($(this).closest('tr').find('td.email').text());
+
+
+			$('#update').on('click', function(){
+				console.log("updated");
+				subtrahend = $('tr').length;
+				counter -= subtrahend;
+			})
 		});
 
 
